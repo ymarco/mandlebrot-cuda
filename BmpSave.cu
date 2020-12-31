ec26677538033f9e24f78c0e939b68d63b3c6d76
Yoav Marco <yoavm448@gmail.com>
2020-12-31 13:03:19 +0200

    Auto-format files

--- BmpSave.cu
+++ BmpSave.cu
@@ -1,29 +1,28 @@
 #include "BmpSave.cuh"
 #include <fstream>
 #include <string>
-namespace BmpSave{
-        __host__ __device__ Color::Color(unsigned char rr,unsigned char gg,unsigned char bb)
-                :r(rr), g(gg), b(bb){};
+namespace BmpSave {
+__host__ __device__ Color::Color(unsigned char rr, unsigned char gg,
+                                 unsigned char bb)
+    : r(rr), g(gg), b(bb){};
 
-        __host__ __device__ Color::Color(int col)
-        {
-                r = (col & 0xFF0000) >> 16;
-                g = (col & 0x00FF00) >> 8;
-                b = col & 0x0000FF;
-        }
-        __host__ __device__ Color::Color()
-        {
-                r = 0;
-                g = 0;
-                b = 0;
-        }
-
-        void saveBmp(const std::string& filename, const int width, const int height, Color* pixels)
-        {
-                BmpHeader header(width, height);
-                std::ofstream fout(filename.c_str(),std::ios::binary);
-                fout.write(reinterpret_cast<char*>(&header), sizeof(BmpHeader));
-                fout.write(reinterpret_cast<char*>(pixels), width * height * sizeof(Color));
-        }
-
+__host__ __device__ Color::Color(int col) {
+  r = (col & 0xFF0000) >> 16;
+  g = (col & 0x00FF00) >> 8;
+  b = col & 0x0000FF;
 }
+__host__ __device__ Color::Color() {
+  r = 0;
+  g = 0;
+  b = 0;
+}
+
+void saveBmp(const std::string &filename, const int width, const int height,
+             Color *pixels) {
+  BmpHeader header(width, height);
+  std::ofstream fout(filename.c_str(), std::ios::binary);
+  fout.write(reinterpret_cast<char *>(&header), sizeof(BmpHeader));
+  fout.write(reinterpret_cast<char *>(pixels), width * height * sizeof(Color));
+}
+
+} // namespace BmpSave
